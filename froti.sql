65223ab7314ea5a78f44c53f7e88b32a26bf48f8
luthfi sugara <sugaraluthfi@gmail.com>
2021-07-24 17:29:21 +0700

    update database

--- froti.sql
+++ froti.sql
@@ -1,76 +1,55 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 12, 2020 at 02:48 PM
--- Server version: 5.7.26
--- PHP Version: 7.4.1
+-- Generation Time: Jul 24, 2021 at 10:28 AM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `historas`
+-- Database: `froti`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `berita`
+-- Table structure for table `tumbuhan`
 --
 
-CREATE TABLE `berita` (
+CREATE TABLE `tumbuhan` (
   `id` int(11) NOT NULL,
   `judul` varchar(100) NOT NULL,
   `konten` text NOT NULL,
-  `penulis` varchar(50) NOT NULL,
-  `id_daerah` int(11) NOT NULL,
   `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `berita`
+-- Dumping data for table `tumbuhan`
 --
 
-INSERT INTO `berita` (`id`, `judul`, `konten`, `penulis`, `id_daerah`, `gambar`) VALUES
-(7, 'test 111', 'test test1111', 'Test1111', 2, '5ee102acb0f85.png'),
-(8, 'test', 'test', 'test', 2, '5ee243ad109a3.jpeg'),
-(9, 'test 123', 'test', 'test', 1, '5ee269f3052ad.jpeg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `daerah`
---
-
-CREATE TABLE `daerah` (
-  `id` int(11) NOT NULL,
-  `daerah` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `daerah`
---
-
-INSERT INTO `daerah` (`id`, `daerah`) VALUES
-(1, 'Medan'),
-(2, 'Binjai');
+INSERT INTO `tumbuhan` (`id`, `judul`, `konten`, `gambar`) VALUES
+(16, 'Berita Tumbuhan', '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<p><img alt=\"\" src=\"http://192.168.100.12:8888/Froti/images/1502922470.jpeg\" style=\"height:675px; width:1200px\" /><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', '60f846bc63eea.jpeg'),
+(17, 'Berita Terbaru', '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<p><img alt=\"\" src=\"http://192.168.100.12:8888/Froti/images/407157065.jpeg\" style=\"height:675px; width:1200px\" /><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', '60f849ae903bb.jpeg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `berita`
+-- Indexes for table `tumbuhan`
 --
-ALTER TABLE `berita`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `daerah`
---
-ALTER TABLE `daerah`
+ALTER TABLE `tumbuhan`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -78,13 +57,12 @@ ALTER TABLE `daerah`
 --
 
 --
--- AUTO_INCREMENT for table `berita`
+-- AUTO_INCREMENT for table `tumbuhan`
 --
-ALTER TABLE `berita`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `tumbuhan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
 
---
--- AUTO_INCREMENT for table `daerah`
---
-ALTER TABLE `daerah`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
